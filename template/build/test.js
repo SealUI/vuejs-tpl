2a50ff1a5e5c597662f90c33e7270e31a8baae65
听着情歌流泪 <develop@lingtal.com>
2017-05-15 15:07:38 +0800

    调整结构

---
+++ template/build/test.js
@@ -0,0 +1,34 @@
+require('./check-versions')()
+
+process.env.NODE_ENV = 'test'
+
+var ora = require('ora')
+var rm = require('rimraf')
+var path = require('path')
+var chalk = require('chalk')
+var webpack = require('webpack')
+var config = require('../config')
+var webpackConfig = require('./webpack.test.conf');
+
+var spinner = ora('开始构建 [测试环境] ...');
+spinner.start()
+
+rm(path.join(config.test.assetsRoot, config.test.assetsSubDirectory), err => {
+  if (err) throw err
+  webpack(webpackConfig, function (err, stats) {
+    spinner.stop()
+    if (err) throw err
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n\n')
+
+    console.log(chalk.green('  测试环境构建完成.\n'))
+    console.log(chalk.yellow(
+      '  提示: 非开发环境，请发布到线上环境进行测试.\n'
+    ))
+  })
+})
